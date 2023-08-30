232f088599245594c877e1625149f43ec77a4a8c
Roque Molina <roque.nicolas.molina@gmail.com>
2023-08-30 02:28:29 -0300

    login

--- lib/auth0/auth0.dart
+++ lib/auth0/auth0.dart
@@ -22,13 +22,26 @@ class Auth0ScreenState extends ConsumerState<Auth0Screen> {
     return Scaffold(
       body: auth0Prov.authStatus == AuthStatus.checking
           ? Container(
-              color: colors.primary,
+              color: const Color(0xff03132a),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset('assets/images/silver-logo_white_font-color.png'),
+                  SizedBox(
+                      width: size.width,
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/images/app_logo.png',
+                            width: kIsWeb ? size.width * .10 : size.width * .30,
+                          ),
+                          Image.asset(
+                            'assets/images/app_logo_letters.png',
+                            width: kIsWeb ? size.width * .30 : size.width * .70,
+                          ),
+                        ],
+                      )),
                   const SizedBox(
-                    height: 30,
+                    height: 10,
                   ),
                   const CircularProgressIndicator(
                     strokeWidth: 4,
@@ -40,149 +53,198 @@ class Auth0ScreenState extends ConsumerState<Auth0Screen> {
           : Container(
               height: size.height,
               width: size.width,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [colors.primary, const Color(0xff23A5CD)]),
+              decoration: const BoxDecoration(
+                color: Color(0xff03132a),
               ),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    kIsWeb
-                        ? const SizedBox(
-                            height: 0,
-                          )
-                        : SizedBox(
-                            height: size.height * .15,
-                          ),
-                    Text(
-                      'Bienvenido!',
-                      style: TextStyle(
-                        color: colors.onPrimary,
-                        fontSize: 50,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Image.asset(
-                        'assets/images/silver-logo_white_font-color.png'),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Text(
-                      'Ingresa',
-                      style: TextStyle(
-                        color: colors.onPrimary,
-                        fontSize: 35,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    auth0Prov.credentials != null
-                        ? ElevatedButton(
-                            onPressed: () {
-                              ref.read(authProvider.notifier).logout();
-                            },
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  const Color(0xFFFFFFFF)),
-                            ),
-                            child: Text(
-                              'Logout ${auth0Prov.credentials!.scopes}',
-                              style: TextStyle(
-                                color: colors.primary,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          )
-                        : ElevatedButton(
-                            onPressed: () {
-                              ref.read(authProvider.notifier).login();
-                            },
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  const Color(0xFFFFFFFF)),
-                            ),
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                color: colors.primary,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                    SizedBox(
-                      height: size.height * .15,
-                    ),
-                    const Text(
-                      '¿Quieres Trabajar con nosotros?',
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      'CONTACTANOS',
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            // Acción al presionar el botón de WhatsApp
-                            // Abre el enlace correspondiente
-                          },
-                          icon: const Icon(
-                            Icons.call,
-                            size: 30,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            // Acción al presionar el botón de Facebook
-                            // Abre el enlace correspondiente
-                          },
-                          icon: const Icon(
-                            Icons.facebook,
-                            size: 30,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            // Acción al presionar el botón de Gmail
-                            // Abre el enlace correspondiente
-                          },
-                          icon: const Icon(
-                            Icons.mail,
-                            size: 30,
-                          ),
-                        ),
-                      ],
-                    ),
-                    /* TextButton(
-                        onPressed: () {
-                          context.push('/details');
-                        },
-                        child: const Text('Ingresa aquíaaa')),
-                    Text(
-                      auth0Prov.authStatus.toString(),
-                      style: const TextStyle(fontSize: 20),
-                    ), */
-                  ]),
+              child: kIsWeb
+                  ? WebLoginView(size: size, colors: colors, ref: ref)
+                  : AppLoginView(
+                      size: size,
+                      colors: colors,
+                      auth0Prov: auth0Prov,
+                      ref: ref),
             ),
     );
+  }
+}
+
+class AppLoginView extends StatelessWidget {
+  const AppLoginView({
+    super.key,
+    required this.size,
+    required this.colors,
+    required this.auth0Prov,
+    required this.ref,
+  });
+
+  final Size size;
+  final ColorScheme colors;
+  final AuthState auth0Prov;
+  final WidgetRef ref;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      SizedBox(
+        height: size.height * .15,
+      ),
+      SizedBox(
+        width: size.width * .70,
+        child: Image.asset('assets/images/silver-logo_white_font-color.png'),
+      ),
+      const SizedBox(
+        height: 10,
+      ),
+      Image.asset('assets/images/login-driving-car.png'),
+      const SizedBox(
+        height: 15,
+      ),
+      Text(
+        '¡Bienvenido a Silver!',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: colors.onPrimary,
+          fontSize: 31,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      const SizedBox(
+        height: 15,
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28.0),
+        child: Text(
+          'Gestiona los viajes corporativos de tus clientes y empleados con un solo clic.',
+          style: TextStyle(
+            color: colors.onPrimary,
+            fontSize: 19,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      const SizedBox(
+        height: 40,
+      ),
+      SizedBox(
+        width: size.width * .7,
+        child: ElevatedButton(
+          onPressed: () {
+            ref.read(authProvider.notifier).login();
+          },
+          child: Text(
+            'Ingresar',
+            style: TextStyle(
+              color: colors.onBackground,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    ]);
+  }
+}
+
+class WebLoginView extends StatelessWidget {
+  const WebLoginView({
+    super.key,
+    required this.size,
+    required this.colors,
+    required this.ref,
+  });
+
+  final Size size;
+  final ColorScheme colors;
+  final WidgetRef ref;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 60),
+              child: Image.asset(
+                'assets/images/silver-logo_white_font-color.png',
+                width: size.width * .20,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: size.width * .50,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 80),
+                child: Text(
+                  '¡Bienvenido a Silver!',
+                  style: TextStyle(
+                    height: 1.2,
+                    color: colors.onPrimary,
+                    fontSize: 70,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: size.width * .50,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 80),
+                child: Text(
+                  'Gestiona los viajes corporativos de tus clientes y empleados con un solo clic.',
+                  style: TextStyle(
+                    color: colors.onPrimary,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: size.width * .5,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 80,
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    ref.read(authProvider.notifier).login();
+                  },
+                  child: Text(
+                    'Ingresar',
+                    style: TextStyle(
+                      color: colors.onBackground,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            )
+          ]),
+      SizedBox(
+        width: size.width * .5,
+        child: Padding(
+          padding: const EdgeInsets.only(right: 100),
+          child: Image.asset(
+            'assets/images/login-driving-car-web.png',
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+      ),
+    ]);
   }
 }

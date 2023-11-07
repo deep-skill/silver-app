034dce156ec0f28850b8e15242669e58e868a3b6
Manuel Mandujano <manduuu2294@gmail.com>
2023-11-07 11:06:10 -0500

    The side menu was adapted for web views (#18)

--- lib/roles/admin/presentation/widgets/side_menu.dart
+++ lib/roles/admin/presentation/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:auth0_flutter/auth0_flutter.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -23,97 +24,189 @@ class SideMenuState extends ConsumerState<SideMenu> {
     final hasNotch = MediaQuery.of(context).viewPadding.top > 20;
     Credentials? credentials = ref.watch(authProvider).credentials;
     final size = MediaQuery.of(context).size;
-
-    return SizedBox(
-      width: size.width * .85,
-      child: NavigationDrawer(
-        backgroundColor: const Color(0xff031329),
-        indicatorColor: const Color(0xff23A5CD),
-        indicatorShape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
-          ),
-        ),
-        onDestinationSelected: (value) {
-          setState(() {
-            navDrawerIndex = value;
-          });
-          final menuItem = appMenuItems[value];
-          menuItem.link == '/admin' ? null : context.push(menuItem.link);
-          widget.scaffoldKey.currentState?.closeDrawer();
-        },
-        selectedIndex: navDrawerIndex,
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, hasNotch ? 20 : 10, 16, 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    widget.scaffoldKey.currentState?.closeDrawer();
-                  },
-                  icon: const Icon(Icons.close),
-                  color: Colors.white,
+    return kIsWeb
+        ? SizedBox(
+            width: size.width * .22,
+            child: NavigationDrawer(
+              backgroundColor: const Color(0xff031329),
+              indicatorColor: const Color(0xff23A5CD),
+              indicatorShape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
                 ),
-                SizedBox(
-                    child: Image.asset(
-                  "assets/images/app_logo.png",
-                  width: size.width * .15,
-                )),
-                const Text('Silver Express',
-                    style: TextStyle(
-                      fontSize: 21,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    )),
-                Text(credentials!.user.email.toString(),
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold)),
+              ),
+              onDestinationSelected: (value) {
+                setState(() {
+                  navDrawerIndex = value;
+                });
+                final menuItem = appMenuItems[value];
+                menuItem.link == '/admin' ? null : context.push(menuItem.link);
+                widget.scaffoldKey.currentState?.closeDrawer();
+              },
+              selectedIndex: navDrawerIndex,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, hasNotch ? 20 : 10, 16, 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 10),
+                      SizedBox(
+                          child: Image.asset(
+                        "assets/images/app_logo.png",
+                        width: size.width * .05,
+                      )),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text('Silver Express',
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )),
+                      Text(credentials!.user.email.toString(),
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ...appMenuItems.map(
+                  (item) => NavigationDrawerDestination(
+                    icon: Icon(item.icon, color: Colors.white),
+                    label: Text(item.title,
+                        style: const TextStyle(color: Colors.white)),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(28, 16, 16, 19),
+                  child: Divider(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextButton(
+                    onPressed: () {
+                      ref.read(authProvider.notifier).logout();
+                    },
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all(const Color(0xff031329)),
+                      shadowColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.logout_rounded,
+                          color: Colors.white,
+                        ),
+                        Text('Cerrar sesión',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
-          ),
-          ...appMenuItems.map(
-            (item) => NavigationDrawerDestination(
-              icon: Icon(item.icon, color: Colors.white),
-              label:
-                  Text(item.title, style: const TextStyle(color: Colors.white)),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(28, 16, 16, 19),
-            child: Divider(),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: TextButton(
-              onPressed: () {
-                ref.read(authProvider.notifier).logout();
+          )
+        : SizedBox(
+            width: size.width * .85,
+            child: NavigationDrawer(
+              backgroundColor: const Color(0xff031329),
+              indicatorColor: const Color(0xff23A5CD),
+              indicatorShape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              onDestinationSelected: (value) {
+                setState(() {
+                  navDrawerIndex = value;
+                });
+                final menuItem = appMenuItems[value];
+                menuItem.link == '/admin' ? null : context.push(menuItem.link);
+                widget.scaffoldKey.currentState?.closeDrawer();
               },
-              style: ButtonStyle(
-                overlayColor:
-                    MaterialStateProperty.all(const Color(0xff031329)),
-                shadowColor: MaterialStateProperty.all(Colors.transparent),
-              ),
-              child: const Row(
-                children: [
-                  Icon(
-                    Icons.logout_rounded,
-                    color: Colors.white,
-                  ),
-                  Text('Cerrar sesión',
-                      style: TextStyle(
+              selectedIndex: navDrawerIndex,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, hasNotch ? 20 : 10, 16, 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          widget.scaffoldKey.currentState?.closeDrawer();
+                        },
+                        icon: const Icon(Icons.close),
                         color: Colors.white,
+                      ),
+                      SizedBox(
+                          child: Image.asset(
+                        "assets/images/app_logo.png",
+                        width: size.width * .15,
                       )),
-                ],
-              ),
+                      const Text('Silver Express',
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )),
+                      Text(credentials!.user.email.toString(),
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                ...appMenuItems.map(
+                  (item) => NavigationDrawerDestination(
+                    icon: Icon(item.icon, color: Colors.white),
+                    label: Text(item.title,
+                        style: const TextStyle(color: Colors.white)),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(28, 16, 16, 19),
+                  child: Divider(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextButton(
+                    onPressed: () {
+                      ref.read(authProvider.notifier).logout();
+                    },
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all(const Color(0xff031329)),
+                      shadowColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.logout_rounded,
+                          color: Colors.white,
+                        ),
+                        Text('Cerrar sesión',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
 

fd052ef0e591f2627fb3e8c5f135fbfb4c2612f2
Manuel Mandujano <manduuu2294@gmail.com>
2023-11-08 09:47:00 -0500

    independent view of the web booking list view

--- lib/roles/admin/presentation/screens/reserve_list_screen.dart
+++ lib/roles/admin/presentation/screens/reserve_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:silverapp/roles/admin/infraestructure/entities/reserve_list.dart';
@@ -32,7 +33,8 @@ class ReserveListViewState extends ConsumerState<ReserveListView> {
   @override
   void initState() {
     super.initState();
-    if (ref.read(reservesListProvider.notifier).currentPage == 0) ref.read(reservesListProvider.notifier).loadNextPage();
+    if (ref.read(reservesListProvider.notifier).currentPage == 0)
+      ref.read(reservesListProvider.notifier).loadNextPage();
   }
 
   @override
@@ -40,80 +42,160 @@ class ReserveListViewState extends ConsumerState<ReserveListView> {
     final reserves = ref.watch(reservesListProvider);
     final size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      child: RefreshIndicator(
-        onRefresh: () => ref.read(reservesListProvider.notifier).reloadData(),
-        child: Column(
-          children: [
-            const SizedBox(height: 15),
-            GestureDetector(
-              onTap: () {
-                final searchedReserves = ref.read(searchedReservesProvider);
-                final searchQuery = ref.read(searchReservesProvider);
+    return kIsWeb
+        ? Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: RefreshIndicator(
+              onRefresh: () =>
+                  ref.read(reservesListProvider.notifier).reloadData(),
+              child: Column(
+                children: [
+                  const SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () {
+                      final searchedReserves =
+                          ref.read(searchedReservesProvider);
+                      final searchQuery = ref.read(searchReservesProvider);
 
-                showSearch<ReserveList?>(
-                        query: searchQuery,
-                        context: context,
-                        delegate: SearchReserveDelegate(
-                            initialReserves: searchedReserves,
-                            searchReserves: ref
-                                .read(searchedReservesProvider.notifier)
-                                .searchReservesByQuery))
-                    .then((reserve) {});
-              },
-              child: SizedBox(
-                  height: size.height * .07,
-                  child: const DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Color(0xffF2F3F7),
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(left: 20),
-                          child: Text(
-                            'Búsqueda de reservas',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                            ),
+                      showSearch<ReserveList?>(
+                              query: searchQuery,
+                              context: context,
+                              delegate: SearchReserveDelegate(
+                                  initialReserves: searchedReserves,
+                                  searchReserves: ref
+                                      .read(searchedReservesProvider.notifier)
+                                      .searchReservesByQuery))
+                          .then((reserve) {});
+                    },
+                    child: SizedBox(
+                        height: size.height * .07,
+                        child: const DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Color(0xffF2F3F7),
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(right: 20),
-                          child: SizedBox(
-                            height: 45,
-                            width: 45,
-                            child: DecoratedBox(
-                              decoration: BoxDecoration(
-                                color: Color(0xff031329),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(12)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 20),
+                                child: Text(
+                                  'Búsqueda de reservas',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 16,
+                                  ),
+                                ),
                               ),
-                              child: Icon(
-                                Icons.search,
-                                color: Colors.white,
-                              ),
-                            ),
+                              Padding(
+                                padding: EdgeInsets.only(right: 20),
+                                child: SizedBox(
+                                  height: 45,
+                                  width: 45,
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: Color(0xff031329),
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(12)),
+                                    ),
+                                    child: Icon(
+                                      Icons.search,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
                           ),
-                        )
-                      ],
-                    ),
-                  )),
+                        )),
+                  ),
+                  const SizedBox(height: 15),
+                  ReservesList(
+                    reserves: reserves,
+                    loadNextPage: () {
+                      ref.read(reservesListProvider.notifier).loadNextPage();
+                    },
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 15),
-            ReservesList(
-              reserves: reserves,
-              loadNextPage: () {
-                ref.read(reservesListProvider.notifier).loadNextPage();
-              },
+          )
+        : Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: RefreshIndicator(
+              onRefresh: () =>
+                  ref.read(reservesListProvider.notifier).reloadData(),
+              child: Column(
+                children: [
+                  const SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () {
+                      final searchedReserves =
+                          ref.read(searchedReservesProvider);
+                      final searchQuery = ref.read(searchReservesProvider);
+
+                      showSearch<ReserveList?>(
+                              query: searchQuery,
+                              context: context,
+                              delegate: SearchReserveDelegate(
+                                  initialReserves: searchedReserves,
+                                  searchReserves: ref
+                                      .read(searchedReservesProvider.notifier)
+                                      .searchReservesByQuery))
+                          .then((reserve) {});
+                    },
+                    child: SizedBox(
+                        height: size.height * .07,
+                        child: const DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Color(0xffF2F3F7),
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 20),
+                                child: Text(
+                                  'Búsqueda de reservas',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(right: 20),
+                                child: SizedBox(
+                                  height: 45,
+                                  width: 45,
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: Color(0xff031329),
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(12)),
+                                    ),
+                                    child: Icon(
+                                      Icons.search,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                  ),
+                  const SizedBox(height: 15),
+                  ReservesList(
+                    reserves: reserves,
+                    loadNextPage: () {
+                      ref.read(reservesListProvider.notifier).loadNextPage();
+                    },
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
   }
 }

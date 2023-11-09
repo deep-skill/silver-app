9f074c629702fdfb3fca0f1f314161a3c2b2a7b2
brunoosuna8 <brunoosuna8@gmail.com>
2023-11-09 12:38:39 -0300

    add:no role welcome screen

--- lib/roles/no_role/no_role_screen.dart
+++ lib/roles/no_role/no_role_screen.dart
@@ -2,6 +2,7 @@ import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:silverapp/providers/auth0_provider.dart';
+import 'package:silverapp/roles/no_role/WelcomeMsgScreen.dart';
 
 class NoRoleScreen extends ConsumerWidget {
   static const name = 'no-role';
@@ -10,30 +11,56 @@ class NoRoleScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Credentials? credentials = ref.watch(authProvider).credentials;
+    final size = MediaQuery.of(context).size;
+    final String? userName = credentials?.user.nickname;
     return Scaffold(
-      appBar: AppBar(title: const Text('No Role - Details Screen auth0')),
+      appBar: AppBar(title: const Text('')),
       body: Column(
         children: [
-          Center(
-            child: ElevatedButton(
-                onPressed: () => print('here'),
-                child: credentials != null
-                    ? Column(
-                        children: [
-                          Text(credentials.accessToken),
-                          Text(credentials.scopes.toString()),
-                        ],
-                      )
-                    : const Text('not logged')),
-          ),
+          SizedBox(height: size.height * .03),
+          credentials != null
+              ? Column(
+                  children: [
+                    // Text(credentials.accessToken),
+                    // Text(credentials.scopes.toString()),
+                    Padding(
+                      padding: EdgeInsets.all(size.width * .08),
+                      child: WelcomeMsgScreen(
+                        mainImage: Image.asset(
+                          'assets/images/app_logo_auth.png',
+                        ),
+                        title: Text(
+                          'Bienvenido/a $userName a Silver Expresss!',
+                          style: TextStyle(fontSize: size.width * .08),
+                          textAlign: TextAlign.center,
+                        ),
+                        subTitle: Text(
+                          'Al parecer todavía no tienes un rol asignado',
+                          style: TextStyle(fontSize: size.width * .05),
+                          textAlign: TextAlign.center,
+                        ),
+                        secSubTitle: Text(
+                          'Por favor, contáctate con un administrador',
+                          style: TextStyle(fontSize: size.width * .03),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              : const Text('not logged'),
           ElevatedButton(
             onPressed: () {
               ref.read(authProvider.notifier).logout();
             },
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.lightBlue),
             ),
-            child: const Text('Logout'),
+            child: const Text(
+              'Logout',
+              style: TextStyle(color: Colors.white),
+            ),
           )
         ],
       ),

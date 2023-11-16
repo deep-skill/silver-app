c5681bebe320b501ee7448737cfc6ccde4e0ca72
brunoosuna8 <brunoosuna8@gmail.com>
2023-11-16 19:15:04 -0300

    fix:screen working completly

--- lib/roles/no_role/no_role_screen.dart
+++ lib/roles/no_role/no_role_screen.dart
@@ -12,65 +12,82 @@ class NoRoleScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     Credentials? credentials = ref.watch(authProvider).credentials;
     final size = MediaQuery.of(context).size;
-    final String? userName = credentials?.user.nickname;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('')),
-      body: Container(
-        // height: context.size?.height,
-        //falta hacer un widget que le pase todo, para poder pasar el size ocmo parametro,sino no me lo renderiza
-        decoration: const BoxDecoration(
-          color: Color(0xffF2F3F7),
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: size.height * .03),
-            credentials != null
-                ? Column(
-                    children: [
-                      // Text(credentials.accessToken),
-                      // Text(credentials.scopes.toString()),
-                      Padding(
-                        padding: EdgeInsets.all(size.width * .08),
-                        child: WelcomeMsgScreen(
-                          mainImage: Image.asset(
-                            'assets/images/app_logo_auth.png',
-                          ),
-                          title: Text(
-                            'Bienvenido/a $userName a Silver Expresss!',
-                            style: TextStyle(fontSize: size.width * .08),
-                            textAlign: TextAlign.center,
-                          ),
-                          subTitle: Text(
-                            'Al parecer todavía no tienes un rol asignado',
-                            style: TextStyle(fontSize: size.width * .05),
-                            textAlign: TextAlign.center,
-                          ),
-                          secSubTitle: Text(
-                            'Por favor, contáctate con un administrador',
-                            style: TextStyle(fontSize: size.width * .03),
-                            textAlign: TextAlign.center,
-                          ),
+        appBar: AppBar(title: const Text('Bienvenido/a')),
+        body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: ShowInformation(
+            size: size,
+            credentials: credentials,
+          ),
+        ));
+  }
+}
+
+class ShowInformation extends ConsumerWidget {
+  final Size size;
+  final Credentials? credentials;
+  final Future? buttonOnpressed;
+  const ShowInformation(
+      {super.key, required this.size, this.credentials, this.buttonOnpressed});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      height: size.height,
+      decoration: const BoxDecoration(
+        color: Color(0xffF2F3F7),
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      child: Column(
+        children: [
+          SizedBox(height: size.height * .03),
+          credentials != null
+              ? Column(
+                  children: [
+                    // Text(credentials.accessToken),
+                    // Text(credentials.scopes.toString()),
+                    Padding(
+                      padding: EdgeInsets.all(size.width * .08),
+                      child: WelcomeMsgScreen(
+                        mainImage: Image.asset(
+                          'assets/images/app_logo_auth.png',
                         ),
-                      )
-                    ],
-                  )
-                : const Text('not logged'),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(authProvider.notifier).logout();
-              },
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.lightBlue),
-              ),
-              child: const Text(
-                'Logout',
-                style: TextStyle(color: Colors.white),
-              ),
-            )
-          ],
-        ),
+                        title: Text(
+                          'Bienvenido/a ${credentials?.user.nickname} a Silver Expresss!',
+                          style: TextStyle(fontSize: size.width * .08),
+                          textAlign: TextAlign.center,
+                        ),
+                        subTitle: Text(
+                          'Al parecer todavía no tienes un rol asignado.',
+                          style: TextStyle(fontSize: size.width * .05),
+                          textAlign: TextAlign.center,
+                        ),
+                        secSubTitle: Text(
+                          'Por favor, contáctate con un administrador.',
+                          style: TextStyle(fontSize: size.width * .03),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              : const Text('not logged'),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(authProvider.notifier).logout();
+            },
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.lightBlue),
+            ),
+            child: const Text(
+              'Logout',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
       ),
     );
   }

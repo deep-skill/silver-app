3b88fda83df17e5ed5773c74f9236acc017a5020
Manuel Mandujano <manduuu2294@gmail.com>
2023-11-23 15:41:26 -0500

    adaptation of the booking detail view for the web (#42)
    * adaptation of the booking detail view for the web
    
    * fix childAspectRatio on ReservesList
    
    ---------
    
    Co-authored-by: Galbfran <francogalbiati984@gmail.com>

--- lib/roles/admin/presentation/widgets/box_estado_reserve_detail.dart
+++ lib/roles/admin/presentation/widgets/box_estado_reserve_detail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class BoxEstadoReserveDetail extends StatelessWidget {
@@ -38,48 +39,86 @@ class BoxEstadoReserveDetail extends StatelessWidget {
       iconStatus = Icons.cached;
     }
 
-    return Row(
-      children: [
-        Icon(
-          iconStatus,
-        ),
-        const SizedBox(width: 8.0),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * .35,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return kIsWeb
+        ? Row(
             children: [
-              Text(
-                label,
-                style: const TextStyle(
-                  color: Color(0xFF23A5CD),
-                  fontFamily: "Monserrat",
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                ),
-                overflow: TextOverflow
-                    .ellipsis, // Muestra "..." cuando el texto se desborda
-                maxLines: 1,
+              Icon(
+                iconStatus,
               ),
-              Text(
-                textStatus,
-                style: TextStyle(
-                  color: colorStatus,
-                  fontFamily: "Monserrat",
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+              const SizedBox(width: 8.0),
+              SizedBox(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      label,
+                      style: const TextStyle(
+                        color: Color(0xFF23A5CD),
+                        fontFamily: "Montserrat-Medium",
+                        fontSize: 12,
+                      ),
+                      overflow: TextOverflow
+                          .ellipsis, // Muestra "..." cuando el texto se desborda
+                      maxLines: 1,
+                    ),
+                    Text(
+                      textStatus,
+                      style: TextStyle(
+                        color: colorStatus,
+                        fontFamily: "Montserrat-Medium",
+                        fontSize: 15,
+                      ),
+                      overflow: TextOverflow
+                          .ellipsis, // Muestra "..." cuando el texto se desborda
+                      maxLines: 1,
+                    ),
+                  ],
                 ),
-                overflow: TextOverflow
-                    .ellipsis, // Muestra "..." cuando el texto se desborda
-                maxLines: 1,
-              ),
-              Container(
-                height: 2.0,
               ),
             ],
-          ),
-        ),
-      ],
-    );
+          )
+        : Row(
+            children: [
+              Icon(
+                iconStatus,
+              ),
+              const SizedBox(width: 8.0),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .35,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      label,
+                      style: const TextStyle(
+                        color: Color(0xFF23A5CD),
+                        fontFamily: "Monserrat",
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow
+                          .ellipsis, // Muestra "..." cuando el texto se desborda
+                      maxLines: 1,
+                    ),
+                    Text(
+                      textStatus,
+                      style: TextStyle(
+                        color: colorStatus,
+                        fontFamily: "Monserrat",
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow
+                          .ellipsis, // Muestra "..." cuando el texto se desborda
+                      maxLines: 1,
+                    ),
+                    Container(
+                      height: 2.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
   }
 }
--- lib/roles/admin/presentation/widgets/box_reserve_detail.dart
+++ lib/roles/admin/presentation/widgets/box_reserve_detail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class BoxReserveDetail extends StatelessWidget {
@@ -14,42 +15,73 @@ class BoxReserveDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        icon != null
-            ? Icon(
-                icon,
-              )
-            : Text(''),
-        const SizedBox(width: 8.0),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * .35,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return kIsWeb
+        ? Row(
             children: [
-              Text(
-                label,
-                style: const TextStyle(
-                  color: Color(0xFF23A5CD),
-                  fontFamily: 'Montserrat-Medium',
-                  fontSize: 12,
-                ),
-              ),
-              Text(text,
-                  maxLines: 2,
-                  style: const TextStyle(
-                    fontFamily: 'Montserrat-Regular',
-                    fontSize: 16,
-                    // fontWeight: FontWeight.bold,
-                    overflow: TextOverflow.ellipsis,
-                  )),
-              Container(
-                height: 0.0,
+              icon != null
+                  ? Icon(
+                      icon,
+                    )
+                  : Text(''),
+              const SizedBox(width: 8.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    label,
+                    style: const TextStyle(
+                      color: Color(0xFF23A5CD),
+                      fontFamily: 'Montserrat-Medium',
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(text,
+                      maxLines: 2,
+                      style: const TextStyle(
+                        fontFamily: 'Montserrat-Medium',
+                        fontSize: 15,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                ],
               ),
             ],
-          ),
-        ),
-      ],
-    );
+          )
+        : Row(
+            children: [
+              icon != null
+                  ? Icon(
+                      icon,
+                    )
+                  : Text(''),
+              const SizedBox(width: 8.0),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .35,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      label,
+                      style: const TextStyle(
+                        color: Color(0xFF23A5CD),
+                        fontFamily: 'Montserrat-Medium',
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(text,
+                        maxLines: 2,
+                        style: const TextStyle(
+                          fontFamily: 'Montserrat-Regular',
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis,
+                        )),
+                    Container(
+                      height: 0.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
   }
 }

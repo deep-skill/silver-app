31d6970844dcc5d70736f367526c3e49948cd012
Roque Molina <roque.nicolas.molina@gmail.com>
2023-11-29 14:51:39 -0500

    fi

--- lib/roles/driver/presentation/screens/driver_screen.dart
+++ lib/roles/driver/presentation/screens/driver_screen.dart
@@ -1,5 +1,3 @@
-import 'package:auth0_flutter/auth0_flutter.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';

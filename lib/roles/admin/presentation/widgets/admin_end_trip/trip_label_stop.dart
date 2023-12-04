77c9d5a5fa86a240a939fdbae710e0589dbcd2c3
Galbfran <francogalbiati984@gmail.com>
2023-12-04 13:47:24 -0500

    fix labe sstopl trip admin

--- lib/roles/admin/presentation/widgets/admin_end_trip/trip_label_stop.dart
+++ lib/roles/admin/presentation/widgets/admin_end_trip/trip_label_stop.dart
@@ -12,22 +12,30 @@ class TripLabelStop extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: AlignmentDirectional.bottomStart,
-      padding: const EdgeInsets.all(5),
-      margin: const EdgeInsets.all(2),
       child: IntrinsicWidth(
         child: Container(
           padding: const EdgeInsets.all(0),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
-            color: Colors.grey[300],
+            color: const Color(0xffF2F3F7),
           ),
           child: Row(
             children: [
-              Icon(
-                Icons.add_location_alt,
-                color: Colors.grey[600],
+              const SizedBox(width: 15),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .27,
+                child: Flexible(
+                  child: Text(
+                    text,
+                    style: const TextStyle(
+                        fontFamily: 'Roboto-Bold',
+                        fontSize: 14,
+                        color: Color(0xff1D192B)),
+                    maxLines: 5,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
               ),
-              Text(text),
             ],
           ),
         ),

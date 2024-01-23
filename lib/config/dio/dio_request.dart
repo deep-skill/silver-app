beb728e44985ddfb1ee4bdf65682aec0f0640097
Franco Galbiati <francogalbiati984@gmail.com>
2024-01-23 10:32:14 -0500

    change dio

--- lib/config/dio/dio_request.dart
+++ lib/config/dio/dio_request.dart
@@ -6,7 +6,7 @@ const dio = getDio;
 Dio getDio(String accessToken) {
   return Dio(BaseOptions(
       baseUrl:
-          'https://${dotenv.env['YOUR_IP']}:${dotenv.env['SERVER_PORT']}/silver-api/',
+          'http://${dotenv.env['YOUR_IP']}:${dotenv.env['SERVER_PORT']}/silver-api/',
       headers: <String, String>{
         'Content-Type': 'application/json',
         'Authorization': 'Bearer $accessToken'
